d7e84516fca6adb0f955414f8eb3a04eaf409cea
jgodoy <jgodoy@gmail.com>
2022-01-28 16:18:51 -0600

    Borre la clase ._.Xd

--- Main.java
+++ Main.java
@@ -4,19 +4,25 @@
  * and open the template in the editor.
  */
 package Laboratorio2;
+
 import java.util.Scanner;
 import java.util.ArrayList;
+
 /**
  *
  * @author jcgof
  */
 public class Main {
-    
-     static ArrayList<Animales> animales;
-    
-    public static void main(String args[]){
-        Animales am = new Animales("TigreDeKungFu","Po","Templado","Bamboo","Es un panda que hace kung fu GUAU","China",10);
-        animales.add(am);
-        System.out.println(animales);
+    static ArrayList<Animales> array = new ArrayList();
+    public static void main(String args[]) {
+        Animales animales = new Animales("Tigresa","Po","Helado", "Kung fu","Diez","test",1);
+        array.add( new Animales("Tigresa","Po","Helado", "Kung fu","Diez","test",1));
+        
+        System.out.println(array);
+     
     }
+    
+    
+    
+    
 }
